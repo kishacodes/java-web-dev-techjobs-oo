eb6cba734ccbbb23d0cde429b37ac05e0ef947ee
Kisha Codes <Kishacodes@gmail.com>
2020-07-15 21:05:55 -0400

    Removed duplicate test in JobTest

--- src/org/launchcode/techjobs_oo/Tests/JobTest.java
+++ src/org/launchcode/techjobs_oo/Tests/JobTest.java
@@ -4,26 +4,26 @@ package org.launchcode.techjobs_oo.Tests;
 import org.junit.Before;
 import org.junit.Test;
 import org.launchcode.techjobs_oo.*;
-
 import static org.junit.Assert.*;
 
-public class JobTest {
-    public Job job1 = new Job();
-    public Job job2 = new Job();
 
+public class JobTest {
     @Test
     public void testSettingJobId() {
+        Job job2 = new Job();
         job2.getId();
         assertEquals(1,1);
     }
     @Test
     public void testSettingFirstJobId() {
+        Job job1 = new Job();
         job1.getId();
         assertEquals(0,0);
     }
     @Test
     public void testJobConstructorSetsAllFields() {
         Job job3 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+
         assertTrue(job3 instanceof Job);
 
     }
@@ -37,24 +37,26 @@ public class JobTest {
         assertFalse(job4.equals(job5));
     }
 //Test toString method
+@Test
 
 
-//    @Test
-//    public void testJobsForToStringCorrectly() {
-//        Job job5 = new Job("software engineer", new Employer("Tidal"), new Location("San Francisco"), new PositionType("Production"), new
-//                CoreCompetency("java certified"));
-//        assertEquals(job5.toString(),"_");
-//    }
-//    @Test
-//    public void testJobsForToStringWithLabelCorrectly() {
-//        Job job5 = new Job("software engineer", new Employer("Tidal"), new Location("San Francisco"), new PositionType("Production"), new
-//                CoreCompetency("java certified"));
-//        assertEquals(job5.toString(),"ID: 3");
-//    }
-//    @Test
-//    public void testJobsForToStringWithNullLabelCorrectly() {
-//        Job job5 = new Job("", new Employer("Tidal"), new Location("San Francisco"), new PositionType("Production"), new
-//                CoreCompetency("java certified"));
-//        assertEquals(job5.toString(),"Name: Data not available.");
-//    }
+public void testToString(){
+    Job job4 = new Job("software engineer", new Employer("Tidal"), new Location("San Francisco"), new PositionType("Production"), new
+            CoreCompetency("java certified"));
+        assertEquals("software engineer", job4.getName());
+}
+
+
+    @Test
+    public void testJobsForToStringWithLabelCorrectly() {
+        Job job5 = new Job("software engineer", new Employer("Tidal"), new Location("San Francisco"), new PositionType("Production"), new
+                CoreCompetency("java certified"));
+        assertEquals("Tidal", job5.getEmployer().getValue());
+    }
+    @Test
+    public void testJobsForToStringWithNullLabelCorrectly() {
+        Job job5 = new Job("", new Employer("Tidal"), new Location("San Francisco"), new PositionType("Production"), new
+                CoreCompetency("java certified"));
+        assertEquals("Data not available.", job5.getName());
+    }
 }
